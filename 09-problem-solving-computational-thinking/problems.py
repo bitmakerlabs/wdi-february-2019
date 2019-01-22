c3e47c2240b8b1e3b4aa3485a32a352de7400372
NatalieBlack <natalievictoriablack@gmail.com>
2019-01-21 21:32:07 -0500

    Convert .rb file to .py for problem solving lesson

---
+++ 09-problem-solving-computational-thinking/problems.py
@@ -0,0 +1,15 @@
+# Problem: Get the total sum of a list of numbers
+# 1 2 5 11 1
+
+
+# Problem: Pig Latin
+# Pig Latin
+ # "apple" => "appleay"
+ # "plum" => "umplay"
+ # grape => apegray
+ # Natalie => atalieNay
+ # "squeeze" => "eezesquay"
+
+# Problem: Find frequency of the letter "s" in a string
+# Problem: Find frequency of the letter "a" in a string
+# Problem: Find the most frequent letter in a string
