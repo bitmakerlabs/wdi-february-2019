6edae9bdcd7117fff69e389baa22cf1ed1ede7ec
NatalieBlack <natalievictoriablack@gmail.com>
2019-02-11 12:07:47 -0500

    In-class code from programming-fundamentals-part-1

---
+++ 06-programming-fundamentals-part-1/instructor/first_python.py
@@ -0,0 +1,69 @@
+# print("Hello world")
+
+
+# print("What's your name?")
+#
+# user_name = input()
+#
+# print("Hi, {}".format(user_name))
+#
+# print("What year were you born?")
+#
+# user_birth_year = int(input())
+#
+# user_age = 2019 - user_birth_year
+#
+# print("You are {} years old".format(user_age))
+
+# print("what day is it?")
+# day_of_week = input()
+#
+# if day_of_week == "Tuesday":
+#   print("Happy Tuesday")
+#
+#   print("Do you like Tuesdays?")
+#   likes_tues = input()
+#   if likes_tues == 'yes' or likes_tues == 'y':
+#       print("Yay")
+#   else:
+#       print("sorry")
+# elif day_of_week == 'Monday':
+#     print("Monday 1")
+# elif day_of_week == 'Monday':
+#     print("Monday 2")
+#
+#
+# print("This always happens")
+#
+#
+count = 10
+
+while count >= 0:
+  print(count)
+  count = count - 1
+
+
+
+print("After the loop")
+
+# password = "please"
+
+# print("What's the password?")
+# user_answer = input()
+#
+# while user_answer != password:
+#     print("What's the password?")
+#     user_answer = input()
+#
+# print("That's right")
+
+
+
+
+
+
+
+
+
+
+
