e407dca087138d6a18ef3d39b244dc877df1a31d
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2019-03-26 12:11:35 -0400

    DOM lecture stuff

--- 31-intro-to-dom/instructor/main.js
+++ 31-intro-to-dom/instructor/main.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(e){
   // CODE GOES HERE
+  let title = document.querySelector('#page-title');
+  title.innerText = "Some other title";
+
+  var ul = document.querySelector('ul');
+
+  for (var i = 1; i <= 1000; i++) {
+    var item = document.createElement('li');
+    item.className = "item";
+    item.innerText = "Item " + i;
+    ul.appendChild(item);
+  }
 })
