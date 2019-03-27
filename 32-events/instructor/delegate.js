f2247d7decc2c66cef3be6b8109fe7969d7ab17d
bettymakes <makewithbetty@gmail.com>
2019-03-27 12:04:12 -0400

    w8d3 js events

--- 32-events/instructor/delegate.js
+++ 32-events/instructor/delegate.js
@@ -6,6 +6,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const container = document.querySelector('#container');
 
+  container.addEventListener('click', function(event) {
+    event.target.remove();
+    console.log("child circle was clicked and removed");
+  });
+
+  // If you wanted to delegate from the `body`, you'll need to take into
+  // consideration of the fact that you only want to be deleting circles
+  // so you'll need to check if the element you clicked on is a circle first
+
+  // const body = document.querySelector('#body').addEventListener('click', function(event) {
+  //   console.log(event);
+  //   if(event.target.classList.contains('circle')) {
+  //     event.target.remove();
+  //   }
+  //
+  //   console.log("clicked on something that wasn't a circle.");
+  // })
+
   const boxMaker = document.querySelector('#box-maker');
 
   boxMaker.addEventListener('click', function() {
@@ -25,6 +43,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
       container.append(circle);
 
+      // This is totally okay 👌 but we'll be creating an infinite amount of
+      // events if the user keeps clicking on the link. So a better way to
+      // achieve this is by delgating the event from the parent element.
+
+      // circle.addEventListener('click', function(event) {
+      //   event.target.remove();
+      //   console.log('circle was removed!!!');
+      // });
+
       console.log('link was clicked');
   });
+
+
+  // ⚠️ This didn't work because the circles didn't exist on page load
+  // and this code only executes once, on page load.
+
+  // const circles = document.querySelectorAll('.circle');
+  //
+  // circles.forEach(function(circle) {
+  //   circle.addEventListener('click', function(event) {
+  //     event.target.remove();
+  //     console.log('circle was removed!!!');
+  //   });
+  // })
 });
--- 32-events/instructor/script.js
+++ 32-events/instructor/script.js
@@ -1,1 +1,62 @@
-// Let's add our first event handlers to this simple document
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Let's add our first event handlers to this simple document
+  // 1. Select the `click-bait` element
+  const clickBait = document.getElementById('click-bait');
+
+  // 2. Add an event listener to clickBait and ...
+  //    implement the callback with an anonymous function
+  clickBait.addEventListener('click', function(event) {
+    alert("Trigger's been clicked!");
+    console.log(event);
+
+    event.preventDefault();
+  });
+
+  // 3. Select the `second-level` element
+  const secondLevel = document.getElementById('second-level');
+
+  // 4. Add an event listener to secondLevel and ...
+  //    implement the callback with a function declation
+  secondLevel.addEventListener('click', handleSecondLevelClick);
+
+  function handleSecondLevelClick(event) {
+    alert("Second level's been clicked too!");
+
+    event.stopPropagation();
+  }
+
+
+  // 5. Select the `first-level` element
+  const firstLevel = document.getElementById('first-level');
+
+  // 6. Add an event listener to the firstLevel and ...
+  //    implement the callback with a function definition
+  const fnDefinition = () => {};
+  const handleFirstLevelClick = function() {
+    alert("First level clicked!!");
+  };
+
+  firstLevel.addEventListener('click', handleFirstLevelClick);
+
+
+  // 7. Check out the `event` object:
+    // => Google MDN addEventListener
+    // => https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
+    // QQ: Our default behaviour of the link routes us to Google, can we stop the default behaviour?
+    // (Check out preventDefault)
+
+    // QQ: What default behavrious will you want to prevent typically? There are mainly 2:
+    // (1: links ... 2. _ _ _ _? (rhymes with torms lol) )
+
+    // QQ: Can we stop the event from bubbling up the DOM?
+    // => https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation
+
+    // QQ: Does order of including our JavaScript matter?
+    // DOMContentLoaded => https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+
+    // Refrence to all browser events: https://developer.mozilla.org/en-US/docs/Web/Events
+
+    // Example from earlier: https://codepen.io/fightingtheboss/pen/BNajOr
+});
