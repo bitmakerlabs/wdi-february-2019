2ef9f551a3447688f0cf25a19230ab2441f54d62
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2019-03-27 21:20:03 -0400

    Add Robot model, basic templates, urls, views, etc

---
+++ 34-ajax-and-json/instructor/robots/robots/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Robot(models.Model):
+  name = models.CharField(max_length=255)
+  address = models.CharField(max_length=255)
+  model_number = models.IntegerField()
+  lasers = models.BooleanField(default=False)
+  japanese = models.BooleanField(default=False)
--- 34-ajax-and-json/instructor/robots/robots/urls.py
+++ 34-ajax-and-json/instructor/robots/robots/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from robots import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.robots_index),
+    path('robots/', views.robots_index),
+    path('robots/<int:id>', views.robots_show),
 ]
