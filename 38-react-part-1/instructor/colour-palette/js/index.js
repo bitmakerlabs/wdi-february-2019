d4555a49cb1002bcc93cdc91fe7113e2322c79c5
juneate <roccopan@gmail.com>
2019-04-04 12:08:49 -0400

    Presentation portion complete

--- 38-react-part-1/instructor/colour-palette/js/index.js
+++ 38-react-part-1/instructor/colour-palette/js/index.js
@@ -1,3 +1,49 @@
 // Include useState (hooks) as part of this project
 const { useState } = React;
 
+const Channel = ({rgb}) => {
+
+  return (
+    <div className="channel">
+      <button type="button" className="btn up">+</button>
+      <input type="text" className="txt" value={rgb} />
+      <button type="button" className="btn down">-</button>
+    </div>
+  );
+};
+
+const Swatch = (props) => {
+
+  let r = props.red;
+  let g = props.green;
+  let b = props.blue;
+
+  const styles = {
+    backgroundColor: `rgb(${r}, ${g}, ${b})`
+  }
+
+  return (
+    <li className="swatch" style={styles}>
+      <div>rgb(</div>
+      <Channel rgb={r} />
+      <Channel rgb={g} />
+      <Channel rgb={b} />
+      <div>);</div>
+    </li>
+  );
+};
+
+
+const Palette = () => {
+
+    return (
+      <ul className="palette">
+        <Swatch red={255} green={0} blue={255} />
+        <Swatch red={255} green={255} blue={0} />
+        <Swatch red={0} green={255} blue={255} />
+      </ul>
+    );
+
+};
+
+ReactDOM.render(<Palette />, document.getElementById('app'));
--- 38-react-part-1/instructor/es6-review/js/main.js
+++ 38-react-part-1/instructor/es6-review/js/main.js
@@ -8,6 +8,8 @@ var student = { name: 'Tim Berners-Lee', grade: 89 };
 let isAvailable = true;
 const elOutput = document.getElementById('output');
 
+const anArray = [];
+const anObject = {}
 
 //-------------------------------------------
 
@@ -17,9 +19,12 @@ function timesTwo(num) {
     return num * 2;
 }
 
-// ES6 function with return
+var someVar = timesTwo(5);
+
+// ES6 function with return ("fat arrow")
 const timesThree = num => num * 3;
 
+let someOtherVar = timesThree(5);
 
 //-------------------------------------------
 
@@ -45,13 +50,26 @@ printToElementES6();
 
 //-------------------------------------------
 
+// ES5
+// var name = student.name;
+// var grade = student.grade;
 
 // ES6 destructuring
 const {name, grade} = student;
 const [first, second] = [student, { name: 'Grace Hopper',  grade: 93 } ];
 
+const fruits = ['apples', 'bananas', 'grapes', 'cherries'];
+
+const [fav1, fav2, ...theRest] = fruits;
+
+
+elOutput.addEventListener('click', ({target}) => {
+    console.log(`You clicked ` + target.innerHTML);
+})
+
+
 // ES6 template literals
-printToElementES6(`${name} recieved ${grade}%`);
+printToElementES6(`${name} recieved ${grade / 100}%`);
 printToElementES6(`${second.name} recieved ${second.grade}%`);
 
 // ES6 spread operator
@@ -70,13 +88,15 @@ var es5string = '<nav>' +
                 '  <ul>' +
                 '<nav>';
 
-var es6string = `<nav>
-                   <ul>
-                     <li><a href="#">Home</a></li>
-                     <li><a href="#">About</a></li>
-                     <li><a href="#">Contact</a></li>
-                   <ul>
-                 <nav>`;
+const isOpen = false;
+const es6string = `
+    <nav>
+        <ul class="${ (isOpen) ? `open` : `close` }">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        <ul>
+    <nav>`;
 
 
 //-------------------------------------------
@@ -87,6 +107,25 @@ if (isAvailable) printToElementES6(`I'm available!`);
 
 // ES6 inline condition
 {isAvailable && printToElementES6(`I'm available!`)}
+
+let sure;
+if (isAvailable)
+    sure = `Yes!`;
+else
+    sure = `Noooo`;
+
+sure = (isAvailable) ? `Yes!` : `Noooo`;
+
+// {isAvailable && 
+
+//     // `<nav>
+//     //     <ul class="${ (isOpen) ? `open` : `close` }">
+//     //         <li><a href="#">Home</a></li>
+//     //         <li><a href="#">About</a></li>
+//     //         <li><a href="#">Contact</a></li>
+//     //     <ul>
+//     // <nav>`
+// }
 
 
 //-------------------------------------------
